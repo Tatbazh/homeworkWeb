084a105acf018b89c3b1e598cfc02ea31cf27937
Tatyana <tatyana.bazh17@gmail.com>
2023-02-03 12:59:12 +0400

    правки после проверки в задаче №2

--- seminar3/2.js
+++ seminar3/2.js
@@ -1,5 +1,5 @@
 //выведение сообщения в console
-let userName = prompt("Введите Ваше имя ");
+const userName = prompt("Введите Ваше имя ");
 function greeting(userName) {
     console.log(`Привет, ${userName}`);
 }
