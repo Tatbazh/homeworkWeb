402a719a43cd2cb5e002e3e5c7740847dd9dd451
Tatyana <tatyana.bazh17@gmail.com>
2023-02-03 12:58:19 +0400

    правки после проверки в задаче №1

--- seminar3/1.js
+++ seminar3/1.js
@@ -1,5 +1,5 @@
-let cels = parseFloat(prompt("Введите температуру в градусах Цельсия "));
+const cels = Number.parseFloat(prompt("Введите температуру в градусах Цельсия "));
 function farengeit(cels) {
     return 9 / 5 * cels + 32;
 }
-alert(`Цельсия ${+cels.toFixed(1)}, Фаренгейт ${+farengeit(cels).toFixed(1)}`);
+alert(`Цельсия ${cels.toFixed(1)}, Фаренгейт ${+farengeit(cels).toFixed(1)}`);
